379af14ef3aa05ee06449dbc280fcf1bbb52b3ad
Pavlina Sikorova <p-sikorova@centrum.cz>
2025-09-02 06:40:56 +0200

    renamed timepix3->detectors

--- src/Reader4D/__init__.py
+++ src/Reader4D/__init__.py
@@ -8,7 +8,7 @@ Created on Mon Sep  1 08:13:42 2025
 __version__ = "0.0.1"
 
 
-import Reader4D.timepix3
+import Reader4D.detectors
 import Reader4D.dtypes
 import Reader4D.convertor
 import Reader4D.analyzer
